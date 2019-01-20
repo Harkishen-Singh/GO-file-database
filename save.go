c7d33da227016c00618e591db67da86e7c79b39d
Harkishen <harkishensingh@hotmail.com>
2019-01-20 13:25:50 +0530

    resumed

--- save.go
+++ save.go
@@ -17,13 +17,14 @@ func filter(text string) (string, bool) {
 		return strings.Replace(text, separator, " ", -1), true
 	}
 	return "", false
+
 }
 
-func collectionStatus(collectionName string, expAddress string) bool {
+func collectionStatus(collectionPath string) bool {
 	// checking current files in warehouse
 
-	expAddress = "warehouse/" + expAddress
-	result, err := exec.Command("ls", expAddress).Output()
+	collectionPath = "warehouse/" + collectionPath
+	result, err := exec.Command("ls", collectionPath).Output()
 	if err != nil {
 		fmt.Println(err)
 		log.Fatal(err)
@@ -34,7 +35,7 @@ func collectionStatus(collectionName string, expAddress string) bool {
 	var checkStatus bool
 
 	for _, element := range existingFiles {
-		if element == collectionName + ".data" {
+		if element == collectionPath + ".data" {
 			checkStatus = true
 			break
 		} else {
@@ -43,13 +44,15 @@ func collectionStatus(collectionName string, expAddress string) bool {
 	}
 
 	return checkStatus
+
 }
 
 func createCollection(address string) bool {
 
 	address = "warehouse/" + address
-	file := ioutil.WriteFile(address, []byte(""), 0777)
+	ioutil.WriteFile(address, []byte(""), 0777)
 	return true
+
 }
 
 func readCollection(address string) (string, bool) {
@@ -57,14 +60,50 @@ func readCollection(address string) (string, bool) {
 	address = "warehouse/" + address
 	file, err := ioutil.ReadFile(address)
 	if err != nil {
-		panic("Error while reading the collection at Address: " + address)
+		fmt.Println("Error while reading the collection at Address: " + address)
 		return "", false
 	}
 	return string(file), true
+
+}
+
+//GetDocuments ...
+func GetDocuments(address string) (string, bool) {
+
+	var documentAvailable = collectionStatus(address)
+	var data string
+	address = "warehouse/" + address
+	if documentAvailable {
+		openfile, err := ioutil.ReadFile(address)
+		if err != nil {
+			data = string(openfile)
+			return data, true
+		}
+		return "ERROR", false
+	}
+	return "DOCUMENT_UNAVAILABLE", false
+
 }
 
 //Save ...
-func Save(path string, data string) {
+func Save(path string, data string) bool {
 
-	exists := collectionStatus(path, path)
+	exists := collectionStatus(path)
+	if exists == false {
+		fmt.Println("No Collection existing at the specified datapath. Creating one ...")
+		createCollection(path)
+	}
+	file, err := os.OpenFile(path, os.O_APPEND, 0600)
+	if err != nil {
+		panic(err)
+	}
+	_, err = file.WriteString(data)
+	if err != nil {
+		fmt.Println("Error occured while writing")
+		fmt.Println(data)
+		fmt.Println("Address: warehouse/"+path)
+		return false
+	}
+	return true
+
 }
