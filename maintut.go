6a6ffd560b271bfd78131b482a1d04e970d3325f
Harkishen <harkishensingh@hotmail.com>
2019-01-20 20:50:05 +0530

    reordered

--- maintut.go
+++ maintut.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/Harkishen-Singh/Go-db/funcs"
 )
 
 func main() {
 	fmt.Println("GO-db is running")
+	// funcs.Save("newTests", "this the the first form of package")
+	funcs.Delete("newTests")
 }
