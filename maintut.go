a2d49552677e43f035b832abda3a074dd8a86d23
muskankhedia <muskan.khedia2000@gmail.com>
2019-01-23 00:35:01 +0530

    -do-

--- maintut.go
+++ maintut.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	// "github.com/Harkishen-Singh/GO-db/gobase"
-	"./gobase"
+	"github.com/Harkishen-Singh/GO-db/gobase"
+	// "./gobase"
 )
 
 func main() {
@@ -16,4 +16,4 @@ func main() {
 	gobase.SaveArr(&app, []string{"Harkishen", "Singh"})
 	gobase.SaveArrFloat64(&app, &b)
 }
-}
+
