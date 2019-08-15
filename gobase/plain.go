1700fbdece6c4cfcadf30de2cd34a17e00238a79
muskankhedia <muskan.khedia2000@gmail.com>
2019-08-15 23:30:18 +0530

    changes done

--- gobase/plain.go
+++ gobase/plain.go
@@ -3,13 +3,14 @@ package gobase
 import (
 	"fmt"
 	"io/ioutil"
-	"os"
-	"strings"
-	"os/exec"
 	"log"
+	"os"
+	"os/exec"
+	"strings"
 )
 
 const separator = ".##."
+
 //EnvironmentPath ...
 var EnvironmentPath = os.Getenv("GOPATH") + "/src/github.com/Harkishen-Singh/GO-file-database/"
 
@@ -35,11 +36,11 @@ func makeDir(name string) bool {
 
 	name = EnvironmentPath + "warehouse/" + name
 	/**
-		* returns the number of directories contained in the gien path [string]
-	*/
+	* returns the number of directories contained in the gien path [string]
+	 */
 	dirOccurence := func(path string) uint16 {
 		var occ uint16
-		for i:= 0; i< len(path); i++{
+		for i := 0; i < len(path); i++ {
 			if path[i] == '/' {
 				occ++
 			}
@@ -48,8 +49,8 @@ func makeDir(name string) bool {
 	}
 
 	/**
-		* creates new directories on name [string]
-	*/
+	* creates new directories on name [string]
+	 */
 	createDirectoriesChain := func(name string) bool {
 		if !checkExistingDir(name) {
 			_, err := exec.Command("mkdir", "-p", name).Output()
@@ -63,8 +64,8 @@ func makeDir(name string) bool {
 	}
 
 	/**
-		* controls the process of creation of chained directories on path [string]
-	*/
+	* controls the process of creation of chained directories on path [string]
+	 */
 	directoryController := func(path string) bool {
 		tempPath := path + "/"
 		createDirectoriesChain(tempPath)
@@ -90,7 +91,7 @@ func collectionStatus(collectionPath string) bool {
 	res := strings.LastIndex(collectionPath, "/")
 	var subPath string
 	if res != -1 {
-		subPath = collectionPath[0: res]
+		subPath = collectionPath[0:res]
 		if !makeDir(subPath) {
 			os.Exit(50005)
 		}
@@ -104,7 +105,7 @@ func collectionStatus(collectionPath string) bool {
 	var checkStatus bool
 
 	for _, element := range existingFiles {
-		if element == collectionPath[res+1:] + ".data" {
+		if element == collectionPath[res+1:]+".data" {
 			checkStatus = true
 			break
 		} else {
@@ -113,7 +114,6 @@ func collectionStatus(collectionPath string) bool {
 	}
 
 	return checkStatus
-
 
 }
 
@@ -126,7 +126,7 @@ func createCollection(address string) bool {
 	_, err := exec.Command("touch", address).Output()
 
 	if err != nil {
-		fmt.Println("Error in createCollection Address: "+address)
+		fmt.Println("Error in createCollection Address: " + address)
 		fmt.Println(err)
 		return false
 	}
@@ -163,14 +163,14 @@ func CollectionsAvailable(address string) ([]string, bool) {
 		path := EnvironmentPath + "warehouse/" + address
 		response, err := exec.Command("ls", path).Output()
 		if err != nil {
-			fmt.Println("Error while looking for Collections, at Address: "+address)
+			fmt.Println("Error while looking for Collections, at Address: " + address)
 			log.Fatal(err)
 		}
 		existingCollections = strings.Split(string(response), "\n")
 	} else {
-		response, err := exec.Command("ls", EnvironmentPath + "warehouse/").Output()
+		response, err := exec.Command("ls", EnvironmentPath+"warehouse/").Output()
 		if err != nil {
-			fmt.Println("Error while looking for Collections, at Address: "+address)
+			fmt.Println("Error while looking for Collections, at Address: " + address)
 			log.Fatal(err)
 		}
 		existingCollections = strings.Split(string(response), "\n")
@@ -202,6 +202,75 @@ func saveCustom(path string, data string, pass uint16) bool {
 	}
 	defer file.Close()
 
+	data = CheckType(pass) + data
+
+	_, err = file.WriteString(data)
+	if err != nil {
+		fmt.Println("Error occurred while writing the following data:")
+		fmt.Println("\n" + data)
+		fmt.Println("Address: warehouse/" + path)
+		fmt.Println(err)
+		return false
+	}
+	return true
+}
+
+//Delete ...
+func Delete(path string) bool {
+
+	path = EnvironmentPath + "warehouse/" + path + ".data"
+	exec.Command("rm", path).Output()
+	var x = strings.LastIndex(path, "/")
+
+	cleanUPEmptyDirectories(path[:x])
+
+	return true
+}
+
+func cleanUPEmptyDirectories(path string) {
+
+	if path[len(path)-9:] != "warehouse" {
+		fmt.Print("inside1")
+		var tt = strings.LastIndex(path, "/")
+		var xx = path[:tt]
+		a, _ := exec.Command("ls", path).Output()
+		var b = strings.Split(string(a), "\n")
+		if xx[len(xx)-9:] != "warehouse" && len(b) != 0 {
+
+			_, err := exec.Command("rm", "-R", xx).Output()
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+		var temp = strings.LastIndex(path, "/")
+		if path[len(path)-10:] != "warehouse" {
+			cleanUPEmptyDirectories(path[:temp])
+		}
+	}
+}
+
+func warehouse() {
+
+	db := "warehouse"
+	resp, err := exec.Command("ls", EnvironmentPath).Output()
+	if err != nil {
+		panic(err)
+	}
+	var checkWarehouse bool
+	var respStringArr = strings.Split(string(resp), "\n")
+	for _, ele := range respStringArr {
+		if ele == "warehouse" {
+			checkWarehouse = true
+			break
+		}
+	}
+	if !checkWarehouse {
+		exec.Command("mkdir", db).Output()
+	}
+
+}
+
+func CheckType(pass uint16) string {
 	var typeVar string
 
 	switch pass {
@@ -242,70 +311,6 @@ func saveCustom(path string, data string, pass uint16) bool {
 	case 12:
 		typeVar = "string"
 	}
-	data = typeVar + data
 
-	_, err = file.WriteString(data)
-	if err != nil {
-		fmt.Println("Error occured while writing the following data:")
-		fmt.Println("\n" + data)
-		fmt.Println("Address: warehouse/" + path)
-		fmt.Println(err)
-		return false
-	}
-	return true
-}
-
-//Delete ...
-func Delete(path string) bool {
-
-	path = EnvironmentPath + "warehouse/" + path + ".data"
-	exec.Command("rm", path).Output()
-	var x = strings.LastIndex(path, "/")
-
-	cleanUPEmptyDirectories(path[:x])
-
-	return true
-}
-
-func cleanUPEmptyDirectories(path string) {
-
-	if path[len(path) - 9:] != "warehouse" {
-		fmt.Print("inside1")
-		var tt = strings.LastIndex(path, "/")
-		var xx = path[:tt]
-		a, _ := exec.Command("ls", path).Output()
-		var b = strings.Split(string(a), "\n")
-		if xx[len(xx) - 9:] != "warehouse" && len(b) != 0 {
-
-			_, err := exec.Command("rm", "-R", xx).Output()
-			if err != nil {
-				fmt.Println(err)
-			}
-		}
-		var temp = strings.LastIndex(path, "/")
-		if path[len(path) - 10:] != "warehouse" {
-			cleanUPEmptyDirectories(path[:temp])
-		}
-	}
-}
-
-func warehouse() {
-
-	db := "warehouse"
-	resp, err := exec.Command("ls", EnvironmentPath).Output()
-	if err != nil {
-		panic(err)
-	}
-	var checkWarehouse bool
-	var respStringArr = strings.Split(string(resp), "\n")
-	for _, ele := range respStringArr {
-		if ele == "warehouse" {
-			checkWarehouse = true
-			break
-		}
-	}
-	if !checkWarehouse {
-		exec.Command("mkdir", db).Output()
-	}
-
+	return typeVar
 }
